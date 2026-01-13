cd55788b6932b2f69c3ddbf7560c3709188f1895
Jairo Villegas <villegasjairo820@gmail.com>
2026-01-13 01:33:45 -0500

    La base de datos del BPI 2012 no sirve para el proyecto, por lo que se camnió por una mas reciente, esto implicó realizar cambios en las rutas y en el notebook de exploracion

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 #Aqui vamos a definir el nombre de las carpetas en las que deseo que se guarden cada uno de los csv
 
-ubicacion_datos_crudos_comprimidos = "C:/Users/jairo/Downloads/BPI Challenge 2012_1_all/BPI_Challenge_2012.xes.gz"
+ubicacion_datos_crudos_comprimidos = "C:/Users/jairo/Downloads/BPI Challenge 2017_1_all/BPI Challenge 2017.xes.gz"
 
 ubicacion_datos_crudos_extraidos = "data/raw/datos_crudos.csv"
